94b64de53673f2ee82c36f31b85a0547d3cc3fbf
Laderlappen99 <takhonglaw@gmail.com>
2024-05-17 13:34:21 +0800

    Added working fan and switch
    Added a working switch with sound feedback to turn on/off the fan blades but currently can only work with raycast

---
+++ Assets/FanRotation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRotation : MonoBehaviour
+{
+    [Tooltip("Max rotation speed")]
+    public float maxRotationSpeed = 500f; // Maximum speed in degrees per second
+    [Tooltip("Acceleration of rotation")]
+    public float acceleration = 50f; // Speed change in degrees per second squared
+    private float currentSpeed = 0f;
+    private bool isOn = false;
+
+    void Update()
+    {
+        float targetSpeed = isOn ? maxRotationSpeed : 0f;
+        currentSpeed = Mathf.MoveTowards(currentSpeed, targetSpeed, acceleration * Time.deltaTime);
+        transform.Rotate(transform.up, currentSpeed * Time.deltaTime);
+    }
+
+    public void TurnOn()
+    {
+        isOn = true;
+    }
+
+    public void TurnOff()
+    {
+        isOn = false;
+
+    }
+}
